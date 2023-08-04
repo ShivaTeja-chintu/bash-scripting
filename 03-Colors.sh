e6951988e11a6cfc5bee608e048a864b7f74f4b8
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-04 23:55:21 +0530

    Auto push

--- 03-Colors.sh
+++ 03-Colors.sh
@@ -11,4 +11,4 @@ Magenta	        35	                   45
 Cyan	        36	                   46
 comment
 # Syntax to print the color --> "echo \e[coloecodeEm your msg will be printed in color\e[0m
-echo \e[32Em shivateja \e[om
+echo \e[32Em shivateja \e[0m
