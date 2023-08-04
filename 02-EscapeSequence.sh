ad9a3268680fdb1e6be7613ca3eb4a65c336f271
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-04 23:54:30 +0530

    Auto push

--- 02-EscapeSequence.sh
+++ 02-EscapeSequence.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 # \n this indicates a new line 
 echo -e "One KG of onion is :\n50 rupees"
 # Whenever you are using special characters, always enclose them in double quotes.
---
+++ 03-Colors.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+<<comment
+Color	Foreground Code (Text)	Background Code
+-------------------------------------------------
+Black	        30	                   40
+Red	            31	                   41
+Green	        32	                   42
+Yellow	        33	                   43
+Blue	        34	                   44
+Magenta	        35	                   45
+Cyan	        36	                   46
+comment
+# Syntax to print the color --> "echo \e[coloecodeEm your msg will be printed in color\e[0m
+echo \e[32Em shivateja \e[om
