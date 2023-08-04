cd8ec4fc517c6c381354c6fc45e5a9a500dd4a18
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-04 23:34:25 +0530

    Auto push

--- 02-EscapeSequence.sh
+++ 02-EscapeSequence.sh
@@ -1,2 +1,6 @@
 # \n this indicates a new line 
 echo -e "One KG of onion is :\n50 rupees"
+# Whenever you are using special characters, always enclose them in double quotes.
+
+# \t it gives a tab space
+echo -e "sl\tname\tMarks\tAvg"
