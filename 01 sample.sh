c6ce4fbe763dbd923e73ac3ba8bc5c80fc25cee0
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-04 15:26:59 +0530

    written a sample printing code

---
+++ 01 sample.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Shebang Notation : This tells the sctipt with interpreter this scripter has to be executed  
+# There can only be one interpreter per script, and that should be always be the 1st line of the script
+
+echo welcome to Bash Scripting
+
+# This is how we can execute the scripts 
+# bash scriptName  or  sh scriptName.sh  or ./scriptName.sh
