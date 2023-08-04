6cee731bcdc04013b8b44a310a4128709e2d82b0
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-05 00:06:22 +0530

    Auto push

--- 03-Colors.sh
+++ 03-Colors.sh
@@ -12,3 +12,7 @@ Cyan	        36	                   46
 comment
 # Syntax to print the color --> echo -e \e[coloecodem your msg will be printed in color\e[0m
 echo -e "\e[33m I am printing yellow color \e[0m"
+
+# To print something with the background with foreground here is the syntax
+# echo -e "\e[30m;36m I am printing this in cyan with red background \e[0m"
+echo -e "\e[30m;36m I am printing this in cyan with red background \e[0m"
