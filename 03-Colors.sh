c2c29fd5e763179e2a635ffb6fcf67c5bcdfd05d
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-05 00:00:09 +0530

    Auto push

--- 03-Colors.sh
+++ 03-Colors.sh
@@ -11,4 +11,4 @@ Magenta	        35	                   45
 Cyan	        36	                   46
 comment
 # Syntax to print the color --> echo -e \e[coloecodem your msg will be printed in color\e[0m
-echo -e "\e[32m shivateja \e[0m"
+echo -e "\e[33m I am printing yellow color \e[0m"
