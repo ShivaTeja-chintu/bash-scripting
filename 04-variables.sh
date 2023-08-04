68066f0bcaf3219d6e7a2a94419f10d9bb936e59
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-05 00:25:47 +0530

    Auto push

---
+++ 04-variables.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# What is a variables???
+# variables are genarally used to store values dynamically
+a = 10
+b = 20
+c =xyz
+
+# There is no concept of data types in linux shel scripting
+# By default everything is a string 
+# if your input or variables are having special charecters enclose them always in double quotes
+
+# How can I print the value of a variable 
+echo a # it will print just 'a'
+echo $a # it will print the value of a .: 10
