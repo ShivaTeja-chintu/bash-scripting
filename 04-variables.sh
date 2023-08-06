ec95a004498af98090b3a8b27769292c8286aa0f
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-06 10:56:14 +0530

    Auto push

--- 04-variables.sh
+++ 04-variables.sh
@@ -11,6 +11,13 @@ c=xyz
 # if your input or variables are having special charecters enclose them always in double quotes
 
 # How can I print the value of a variable 
-echo b # it will print just 'a'
-echo $b # it will print the value of a .: 10
+echo b # it will print just 'b'
+echo $b # it will print the value of b .: 20
 echo ${b} #echo $b and echo ${b} both are same using the flower brackets is preferable
+echo "printing the value of x : ${x}" # it dosen't print anything because we didn't declared the value of x
+
+# Data_Dir = robot
+# rm -rf data/Data_Dir --> it will delete the robot dir
+# B U T what if :
+# Data_Dir is not declared 
+# rm -rf data/Data_Dir --> it will delete total data directory because Data_Dir is empty
