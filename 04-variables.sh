97e12102b9d31c44cc9e521c98dbc95598006015
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-06 12:46:36 +0530

    Auto push

--- 04-variables.sh
+++ 04-variables.sh
@@ -11,10 +11,10 @@ c=xyz
 # if your input or variables are having special charecters enclose them always in double quotes
 
 # How can I print the value of a variable 
-echo b # it will print just 'b'
-echo $b # it will print the value of b .: 20
-echo ${b} #echo $b and echo ${b} both are same using the flower brackets is preferable
-echo "printing the value of x : ${x}" # it dosen't print anything because we didn't declared the value of x
+echo printing b # it will print just 'b'
+echo printing the value of b : $b # it will print the value of b .: 20
+echo printing the value of b  :${b} #echo $b and echo ${b} both are same using the flower brackets is preferable
+echo "printing the value of x :${x}" # it dosen't print anything because we didn't declared the value of x
 
 # Data_Dir = robot
 # rm -rf data/Data_Dir --> it will delete the robot dir
