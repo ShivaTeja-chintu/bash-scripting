b8a9010044c4685d677e609b5b77d72a59023585
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-07 23:38:38 +0530

    Auto push

--- 08-quots.sh
+++ 08-quots.sh
@@ -7,7 +7,14 @@ echo The given arguments count is  :    "$#"
 echo The given arguments are :          "$*"
 echo The given arguments are :          "$@"
 # ------------------------------------------
-# out put 
+# out put
+# -------- 
+# The current process ID is        : 1623
+# The exit code of last command is : 0
+# The given arguments count is     : 0
+# The given arguments are          : 
+# The given arguments are          :
+
 # -------
 
 
@@ -17,3 +24,14 @@ echo The exit code of last command is : '$?'
 echo The given arguments count is  :    '$#'
 echo The given arguments are :          '$*'
 echo The given arguments are :          '$@'
+# output
+# -------
+# The current process ID is :             $$
+# The exit code of last command is :      $?
+# The given arguments count is :          $#
+# The given arguments are :               $*
+# The given arguments are :               $@
+
+# Single quots will supress the power of special variables 
+# whenever you are executing anty special vriables preffer to avoid single quots 
+
---
+++ 09-read.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+read -p "Enter your name : " name
+
+echo -e "The name of the person who ran the script is : $name" 
+
