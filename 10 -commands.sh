9e080f627f44a813c91859c3a3f9e054c93c4c71
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-07 23:51:47 +0530

    Auto push

---
+++ 10 -commands.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# there are 4 types of commands 
+
+# 1) Binary         (/bin , /sbin)
+# 2) Aliases        (Alises are shortcuts ,aliascnet="netstat -tulppn")
+# shellBuilt-in commands 
+#4)Functions        # Functions are nothing but a set of commands that can be written in sequence and can be called n number of times 
+
+# How to declare a function ?
+
+f(){
+    echo hi 
+}
+# this is how we call a function
+f 
