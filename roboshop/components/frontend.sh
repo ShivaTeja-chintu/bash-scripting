170fbcf79e4a1455d4e1990edffefc7d072bbc79
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-09 23:10:13 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -54,3 +54,10 @@ if [ $? -eq 0 ]; then
 else 
     echo -e "\e[31m Failure \e[0m]"
 fi
+echoho -n " Starting Nginx : "
+$(systmctl start nginx)
+if [ $? -eq 0 ]; then
+    echo -e "\e[32m Success \e[0m]"
+else 
+    echo -e "\e[31m Failure \e[0m]"
+fi
