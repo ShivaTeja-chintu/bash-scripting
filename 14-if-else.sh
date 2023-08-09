b3c0393a6a38d441be98c996a4e4c9d0c22d2727
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-09 18:56:11 +0530

    Auto push

---
+++ 14-if-else.sh
@@ -0,0 +1,10 @@
+# If condition is ideally available in three formats. :
+# simple if 
+# if else 
+# else if
+
+echo demo on if condition with string 
+Action= $1
+if [ "Action"="start" ] then
+    echo startting the payment 
+fi
