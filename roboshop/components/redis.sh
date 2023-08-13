b6f855c779353cd8a68652e2819f19136fca1639
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-13 16:36:44 +0530

    Auto push

--- roboshop/components/redis.sh
+++ roboshop/components/redis.sh
@@ -29,6 +29,7 @@ status $?
 
 echo -n Enabling the ${component} visibility :
 sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/${component}.conf
+sed -ie 's/127.0.0.1/0.0.0.0/g' /etc/${component}/${component}.conf
 status $?
 
 echo -n daemon-reloading the ${component} :
@@ -38,24 +39,4 @@ echo -n restarting the ${component} :
 systemctl restart ${component}
 status $?
 
-# echo -n Starting ${component} :
-# systemctl enable ${component} &>> {LogFile}
-# systemctl start ${component} &>> {LogFile}
-# status $?
-
-
-# echo -n downloading the schema : 
-# curl -s -L -o /tmp/mongodb.zip "https://github.com/stans-robot-project/mongodb/archive/main.zip"
-# status $?
-
-# echo -n Extracting the ${component} schema : 
-# cd /tmp
-# unzip -o ${component}.zip &>> {LogFile} # Here -O represents overwright
-# status $?
-
-# echo -n Injecting the ${component} schema :
-# cd ${component}-main
-# mongo < catalogue.js &>> {LogFile}
-# mongo < users.js &>> {LogFile}
-# status $?
 echo -e "\e[35m Installation of ${component} is completed \e[0m"
