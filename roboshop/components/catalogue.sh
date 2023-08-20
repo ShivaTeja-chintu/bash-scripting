d652f887ad04a73e9b48545f7b5a5f53c167a3d0
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-20 13:08:04 +0530

    Auto push

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,6 +1,6 @@
 #!bin/bash
 #set -e # Enable immediate exit on error We need to use this command in every beginning of the script file
-component=catalogue
+COMPONENT=catalogue
 
 source components/common.sh
 
