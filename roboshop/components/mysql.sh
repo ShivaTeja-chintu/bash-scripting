e4fc00b980a009e2acad308c29d6161512085d2b
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-20 16:58:43 +0530

    Auto push

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -27,6 +27,6 @@ stat $?
 echo "show databases;" | mysql -uroot -pRoboShop@1 &>>  ${LOGFILE}
 if [ $? -ne 0 ]; then 
     echo -n "Performing default password reset of root account:"
-    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$DEFAULT_ROOT_PASSWORD &>>  ${LOGFILE}
+    echo "ALTER USER 'root'@'localhost' IDENTIFIED BY 'RoboShop@1'" | mysql  --connect-expired-password -uroot -p$default_root_password &>>  ${LOGFILE}
     stat $?
 fi 
