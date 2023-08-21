259fb4d55f0998af149d45c662977e14624f9a0e
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-21 23:35:06 +0530

    Auto push

--- roboshop/components/frontend.sh
+++ roboshop/components/frontend.sh
@@ -1,99 +1,66 @@
-#!bin/bash
-# 01-${component}
-set -e # Enable immediate exit on error We need to use this command in every beginning of the script file
-<<comment
-The ${component} is the service in RobotShop to serve the web content over Nginx.
+#!/bin/bash 
 
-Install Nginx.
+# Validate the user who is running the script is a root user or not.
 
-```
+USER_ID=$(id -u)
+COMPONENT=frontend
+LOGFILE="/tmp/${COMPONENT}.log"
 
-# yum install nginx -y
-# systemctl enable nginx
-# systemctl start nginx
+if [ $USER_ID -ne 0 ] ; then    
+    echo -e "\e[31m Script is expected to executed by the root user or with a sudo privilege \e[0m \n \t Example: \n\t\t sudo bash wrapper.sh frontend"
+    exit 1
+fi 
 
-```
-
-Let's download the HTDOCS content and deploy it under the Nginx path.
-
-```
-# curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip"
-
-```
-
-Deploy in Nginx Default Location.
-
-```
-# cd /usr/share/nginx/html
-# rm -rf *
-# unzip /tmp/${component}.zip
-# mv ${component}-main/* .
-# mv static/* .
-# rm -rf ${component}-main README.md
-# mv localhost.conf /etc/nginx/default.d/roboshop.conf
-
-```
-
-- Finally, restart the service once to effect the changes.
-- Now, you should be able to access the ROBOSHOP e-commerce webpage as shown below
-
-comment
-######################## Lets start the code ####################################
-component=frontend
-LogFile=/tmp/${component}.log
-status(){
-    if [ $1 -eq 0 ]; then
-    echo -e "\e[32m Success \e[0m"
-else 
-    echo -e "\e[31m Failure \e[0m"
-    exit 2
-fi
+stat() {
+    if [ $1 -eq 0 ]; then 
+        echo -e "\e[32m success \e[0m"
+    else 
+        echo -e "\e[31m failure \e[0m"
+        exit 2
+    fi
 }
 
-# installing  Nginx
-#Validate the user who is running the script Is a root user or not
-USER_ID=$(id -u)
-if [ $USER_ID -ne 0 ] ; then # root user id is always 0 
-    echo -e "\e[32m Script is expected to execute by the root user or with a sudo privilege. \e[0m\nExample: \n\t sudo bash wrapper.sh"
-    exit 1
-fi
+echo -e "\e[35m Configuring ${COMPONENT} ......! \e[0m \n"
 
-echo -e "\e[35m Configuring ${component}..... \e[0m" #we created a component variable in line no.42
+echo -n "Installing Nginx :"
+yum install nginx -y     &>>  ${LOGFILE}
+stat $?
 
+echo -n "Starting Nginx:" 
+systemctl enable nginx   &>>  ${LOGFILE}
+systemctl start nginx    &>>  ${LOGFILE}
+stat $?
 
-echo -n "Installing Nginx : "  #Here we are using -n because after printing this line the cusor dont go to next line 
-yum install nginx -y &>> ${LogFile}  # This command is push the output to ${component}.log file
-status $? #We are calling the status function and passing $? as an argumentt
+echo -n "Downloading the ${COMPONENT} component:"
+curl -s -L -o /tmp/frontend.zip "https://github.com/stans-robot-project/${COMPONENT}/archive/main.zip" 
+stat $? 
 
-echo -n "Enabling Nginx :"
-systemctl enable nginx &>> ${LogFile} # we created a LogFile variable in line no.43
-status $?
+echo -n "Clean up of ${COMPONENT} : "
+cd /usr/share/nginx/html    
+rm -rf *     &>>  ${LOGFILE}
+stat $?
 
-echo -n " Starting Nginx : "
-systemctl start nginx
-status $?
-
-echo -n "Downloading the ${component} Component :"
-curl -s -L -o /tmp/${component}.zip "https://github.com/stans-robot-project/${component}/archive/main.zip"
-status $?
-
-echo -n "Cleaning up of ${component} : "
-cd /usr/share/nginx/html 
-rm -rf * &>> ${LogFile}
-status $?
-
-echo -n "Unzipping the ${component}: "
-unzip /tmp/${component}.zip  &>> ${LogFile}
-status $?
-
-echo -n "Sorting the ${component} files : "
-mv ${component}-main/* .
-mv static/* .
-rm -rf static README.md
+echo -n "Extracting ${COMPONENT} :"
+unzip /tmp/${COMPONENT}.zip     &>>  ${LOGFILE}
+mv ${COMPONENT}-main/*  .
+mv static/* . 
+rm -rf ${COMPONENT}-main README.md
 mv localhost.conf /etc/nginx/default.d/roboshop.conf
-status $?
+stat $?
 
-echo -n "Restarting the Nginx : "
-systemctl daemon-reload
-systemctl restart nginx
-status $?
+echo -n "Updating the Backend Components in the reverse proxy file:"
+
+for component in catalogue user cart shipping payment ; do 
+    sed -i -e "/${component}/s/localhost/${component}.roboshop.internal/" /etc/nginx/default.d/roboshop.conf
+done 
+
+echo -n "Restarting ${COMPONENT}:"
+systemctl daemon-reload     &>>  ${LOGFILE}
+systemctl restart nginx     &>>  ${LOGFILE}
+stat $?
+
+echo -e "\e[35m ${COMPONENT} Installation Is Completed \e[0m \n"
+
+
+# I want to ensure, that SCRIPT SHOULD Fail the user who run the scipt is not a root user.
+# rather executing the commands and failing.
