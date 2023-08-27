1105c1d6bced236226e220ffb4197a3afdca5706
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-27 10:37:26 +0530

    Auto push

---
+++ roboshop/create-ec2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# To create a server, what all information needed 
+# 1) AMI ID 
+
+# 2) Type of instance 
+
+# 3 ) Security Group
+
+# 4) Instances you needed
+
+# 5) DNS Record : Hosted Zone ID 
+COMPONENT=$1
+AMI_ID="ami-0e9fc91dd15aae68b"
+INSTANCEE_TYPE="t3.micro"
+SECURITY_GROUP="sg-05dd1814fb94730c8"
+
+$ aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --key-name MyKeyPair --security-group-ids ${SECURITY_GROUP} --tag-specifications 'ResourceType=instance,Tags=[{Key=webserver,Value=${COMPONENT}}]' 
