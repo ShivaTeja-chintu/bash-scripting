0dd39715e3a868a634eb0649a47544062b798e2a
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-27 15:25:26 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -23,15 +23,15 @@ INSTANCEE_TYPE="t3.micro"
 HOSTEDZONE_ID="Z09543702TR3R8XKVSHJQ"
 SECURITY_GROUP="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=RoboShopAllowAll | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"     
 echo ${SECURITY_GROUP}
-# echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
+echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
 
-# PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT} ${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' ) 
-# echo $PRIVATE_IP
-# echo -e "creating DNS of \e[31m ${COMPONENT} \e[0m ..."
+PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT} ${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' ) 
+echo $PRIVATE_IP
+echo -e "creating DNS of \e[31m ${COMPONENT} \e[0m ..."
 
-# sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json  > /tmp/r53.json 
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json  > /tmp/r53.json 
 
-# aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
 
 
-# echo -e "\e[35m Private IP address of the ${COMPONENT} is created and ready to use it \e[0m"
+echo -e "\e[35m Private IP address of the ${COMPONENT} is created and ready to use it \e[0m"
