f5ce40047c970bc128ae67fd36e51d327e3e9f4f
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-27 15:17:19 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -1,32 +1,92 @@
-#!/bin/bash
-
-# To create a server, what all information needed 
-# 1) AMI ID 
-
-# 2) Type of instance 
-
-# 3 ) Security Group
-
-# 4) Instances you needed
-
-# 5) DNS Record : Hosted Zone ID 
-COMPONENT=$1
-if [ -z $1 ]; then 
-    echo -e "\e[31m component name needed \e[0m"
-    echo -e "\e[35mExample usage : - \n\t\e[0m sudo bash launch-ec2"
+if [ -z $1 ] || [ -z $2 ]  ; then 
+    echo -e "\e[31m COMPONENT NAME IS NEEDED \e[0m \n \t \t"
+    echo -e "\e[35m Ex Usage \e[0m \n\t\t $ bash launch-ec2.sh shipping"
     exit 1
-fi
-AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
-INSTANCEE_TYPE="t3.micro"
-HOSTEDZONE_ID="Z09543702TR3R8XKVSHJQ"
-SECURITY_GROUP=SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=RoboShopAllowAll | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"     
+fi 
+
+# AMI_ID="ami-0c1d144c8fdd8d690"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=b55-allow-all | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"       # b54-allow-all security group id
 echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
-PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT} ${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' ) 
-echo ${Private}
+PRIVATEIP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCE_TYPE}  --security-group-ids ${SG_ID} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}-${ENV}}]" | jq '.Instances[].PrivateIpAddress'| sed -e 's/"//g') 
 
-echo -e "creating DNS of \e[31m ${COMPONENT} \e[0m ..."
-sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json  > /tmp/r53.json 
-aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
+echo -e "Private IP Address of the $COMPONENT-${ENV} is $PRIVATEIP \n\n"
+echo -e "Creating DNS Record of ${COMPONENT}: "
+
+sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATEIP}/" route53.json  > /tmp/r53.json 
+
+aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONEID --change-batch file:///tmp/r53.json
+echo -e "\e[36m **** Creating DNS Record for the $COMPONENT has completed **** \e[0m \n\n"
 
 
-echo -e "\e[35m Private IP address of the ${COMPONENT} is created and ready to use it \e[0m"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+# # To create a server, what all information needed 
+# # 1) AMI ID 
+
+# # 2) Type of instance 
+
+# # 3 ) Security Group
+
+# # 4) Instances you needed
+
+# # 5) DNS Record : Hosted Zone ID 
+# COMPONENT=$1
+# if [ -z $1 ]; then 
+#     echo -e "\e[31m component name needed \e[0m"
+#     echo -e "\e[35mExample usage : - \n\t\e[0m sudo bash launch-ec2"
+#     exit 1
+# fi
+# AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
+# INSTANCEE_TYPE="t3.micro"
+# HOSTEDZONE_ID="Z09543702TR3R8XKVSHJQ"
+# SECURITY_GROUP=SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=RoboShopAllowAll | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"     
+# echo -e "****** Creating \e[35m ${COMPONENT} \e[0m Server Is In Progress ************** "
+# PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT} ${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' ) 
+# echo ${Private}
+
+# echo -e "creating DNS of \e[31m ${COMPONENT} \e[0m ..."
+# sed -e "s/COMPONENT/${COMPONENT}-${ENV}/"  -e "s/IPADDRESS/${PRIVATE_IP}/" route53.json  > /tmp/r53.json 
+# aws route53 change-resource-record-sets --hosted-zone-id $HOSTEDZONE_ID --change-batch file:///tmp/r53.json
+
+
+# echo -e "\e[35m Private IP address of the ${COMPONENT} is created and ready to use it \e[0m"
