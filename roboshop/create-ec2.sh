1fcfac92883b471a8baa633aed6a6cc3164b4331
ShivaTeja-chintu <shivatejas122@gmail.com>
2023-08-27 12:41:06 +0530

    Auto push

--- roboshop/create-ec2.sh
+++ roboshop/create-ec2.sh
@@ -16,10 +16,9 @@ if [ -z $1 ]; then
     echo -e "\e[35mExample usage : - \n\t\e[0m sudo bash launch-ec2"
     exit 1
 fi
-AMI_ID="ami-0e9fc91dd15aae68b"
+AMI_ID="$(aws ec2 describe-images --filters "Name=name,Values=DevOps-LabImage-CentOS7-Backup"| jq ".Images[].ImageId" | sed -e 's/"//g')" 
 INSTANCEE_TYPE="t3.micro"
-SECURITY_GROUP="sg-05dd1814fb94730c8"
-echo -e "*******Creating \e[35m ${COMPONENT}\e[0m server is in progress*******"
+SECURITY_GROUP=SG_ID="$(aws ec2 describe-security-groups  --filters Name=group-name,Values=RoboShopAllowAll | jq '.SecurityGroups[].GroupId' | sed -e 's/"//g')"     
 PRIVATE_IP=$(aws ec2 run-instances --image-id ${AMI_ID} --instance-type ${INSTANCEE_TYPE} --security-group-ids ${SECURITY_GROUP} --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT} ${ENV}}]" | jq '.Instances[].PrivateIpAddress' | sed -e 's/"//g' ) 
 
 echo Private IP address of ec2 is : ${PRIVATE_IP}
